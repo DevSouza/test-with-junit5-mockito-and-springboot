d79aa5fce5712b31bcabfabbed22049ced5ae102
Andre Souza <devsouza01@gmail.com>
2024-02-29 16:12:49 -0300

    criando a a classe `UserResource` com o metodo `findById`

--- src/main/java/br/com/devsouza/api/resources/UserResource.java
+++ src/main/java/br/com/devsouza/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.devsouza.api.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devsouza.api.domain.User;
+import br.com.devsouza.api.services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Integer id){
-		
-		return ResponseEntity.ok().body(new User(1, "Valdir", "val@gmail.com", "123"));
+		return ResponseEntity.ok().body(userService.findById(id));
 	}
 	
 }
