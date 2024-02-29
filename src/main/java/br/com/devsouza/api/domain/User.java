f0a86466498085e6c2752f68d2631004d6625bb0
Andre Souza <devsouza01@gmail.com>
2024-02-29 15:53:47 -0300

    criando a classe `User`

---
+++ src/main/java/br/com/devsouza/api/domain/User.java
@@ -0,0 +1,30 @@
+package br.com.devsouza.api.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+}
