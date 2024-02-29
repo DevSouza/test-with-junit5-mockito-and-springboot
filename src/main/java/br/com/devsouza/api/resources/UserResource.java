2592a05a7d517ae92f0086ff857dec7dd77bd208
Andre Souza <devsouza01@gmail.com>
2024-02-29 16:52:11 -0300

    Criando o endpoint `/user` que retorna todos usuários

--- src/main/java/br/com/devsouza/api/resources/UserResource.java
+++ src/main/java/br/com/devsouza/api/resources/UserResource.java
@@ -1,5 +1,7 @@
 package br.com.devsouza.api.resources;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.devsouza.api.domain.User;
 import br.com.devsouza.api.domain.dto.UserDTO;
 import br.com.devsouza.api.services.UserService;
 
@@ -24,6 +27,12 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 		return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		return ResponseEntity.ok().body(userService.findAll()
+					.stream().map(x -> mapper.map(x, UserDTO.class)).toList());
 	}
 	
 }
--- src/main/java/br/com/devsouza/api/services/UserService.java
+++ src/main/java/br/com/devsouza/api/services/UserService.java
@@ -1,9 +1,12 @@
 package br.com.devsouza.api.services;
 
+import java.util.List;
+
 import br.com.devsouza.api.domain.User;
 
 public interface UserService {
 
 	User findById(Integer id);
+	List<User> findAll();
 	
 }
